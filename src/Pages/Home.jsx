6b7cc95450dc9f9a5893cca559d4682db6b86c6c
akshayKocharekar <akshaykocharekar3@gmail.com>
2025-05-25 17:55:08 +0530

    fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { AnimatedTestimonials } from "../Components/AnimatedTestimonials";
-import {
-  TypewriterEffect,
-  TypewriterEffectSmooth,
-} from "../components/TypewriterEffect";
+import { TypewriterEffectSmooth } from "../components/TypewriterEffect";
 
 const words = [
   { text: "Celebrate" },
